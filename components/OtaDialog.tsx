de48b86d42224c8513d7fa0fb9026041faa6d9f2
freethenation <rpklafter@yahoo.com>
2023-09-18 13:08:23 -0400

    make OTA dialog work

--- components/OtaDialog.tsx
+++ components/OtaDialog.tsx
@@ -10,52 +10,62 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import useSWR from 'swr';
+import { post } from '../misc/fetchers';
+import { mutate } from 'swr';
 
 import { Client, Release } from '../misc/model';
 
 export default function OtaDialog({
-  client,
-  open,
-  onClose,
+    client,
+    open,
+    onClose,
 }: {
-  client: Client;
-  open: boolean;
-  onClose: (event: any) => void;
+    client: Client;
+    open: boolean;
+    onClose: (event: any) => void;
 }) {
-  const { data: releaseData, error } = useSWR<Release[]>(
-    '/api/releases/?refresh=true'
-  );
-  const [release, setRelease] = React.useState<string>('');
+    const { data: releaseData, error } = useSWR<Release[]>(
+        '/api/releases/github/?refresh=true'
+    );
+    const [release, setRelease] = React.useState<string>('');
 
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Flash Release OTA</DialogTitle>
-      <DialogContent>
-        <DialogContentText sx={{ marginBottom: 2 }}>
-          Select a release to update the client &quot;
-          {client.label || client.hostname}&quot; OTA
-        </DialogContentText>
-        <FormControl fullWidth>
-          <InputLabel id="release-label">Release</InputLabel>
-          <Select
-            labelId="release-label"
-            fullWidth={true}
-            value={release}
-            label="Release"
-            onChange={(event) => setRelease(event.target.value as string)}
-          >
-            {releaseData?.map((release) => (
-              <MenuItem key={release.id} value={release.name}>
-                {release.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={onClose}>Flash Release</Button>
-      </DialogActions>
-    </Dialog>
-  );
+    async function onFlash(event: any) {
+        const releaseToOta = releaseData?.find((r) => r.name === release);
+        await post('/api/ota', { ota_url: releaseToOta?.assets[0].url, hostname: client.hostname });
+        await mutate('/api/clients'); //OTA update is very async so this won't really work but better than nothing
+        setTimeout(() => mutate('/api/clients'), 30*1000) //hackz to make it work. Mutate in 30 seconds so we catch changes
+        onClose(event)
+    }
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Flash Release OTA</DialogTitle>
+            <DialogContent>
+                <DialogContentText sx={{ marginBottom: 2 }}>
+                    Select a release to update the client &quot;
+                    {client.label || client.hostname}&quot; OTA
+                </DialogContentText>
+                <FormControl fullWidth>
+                    <InputLabel id="release-label">Release</InputLabel>
+                    <Select
+                        labelId="release-label"
+                        fullWidth={true}
+                        value={release}
+                        label="Release"
+                        onChange={(event) => setRelease(event.target.value as string)}
+                    >
+                        {releaseData?.map((release) => (
+                            <MenuItem key={release.id} value={release.name}>
+                                {release.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button onClick={onFlash}>Flash Release</Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
