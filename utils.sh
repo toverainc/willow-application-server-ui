93ecc55b5d7513f2eb4472b0334ed86cb15c7994
Kristian Kielhofner <kris@tovera.com>
2023-09-21 19:08:25 -0500

    Use container name as a hacky lock mechanism to prevent invocations from conflicting

--- utils.sh
+++ utils.sh
@@ -4,6 +4,8 @@ set -e
 TAG="willow-application-server-ui:latest"
 NEXT_DEV_PORT="3000"
 
+NAME="wasui"
+
 if [ -r .env ]; then
     echo "Using configuration overrides from .env file"
     . .env
@@ -34,7 +36,7 @@ case $1 in
 build)
     # Hack to not include our environment in build
     user_env disable
-    docker run --rm -it -v "$PWD":/was-ui "$TAG" npm run build
+    docker run --rm -it -v "$PWD":/was-ui --name "$NAME" "$TAG" npm run build
     user_env enable
     if [ "$WAS_DIR" ]; then
         WAS_ADMIN_DIR="$WAS_DIR/static/admin"
@@ -52,17 +54,17 @@ build-docker|docker-build)
 ;;
 
 install)
-    docker run --rm -it -v "$PWD":/was-ui "$TAG" npm install
+    docker run --rm -it -v "$PWD":/was-ui --name "$NAME" "$TAG" npm install
 ;;
 
 run)
     echo "Use HTTP port $NEXT_DEV_PORT to access development server"
-    docker run --rm -it -v "$PWD":/was-ui -p "$NEXT_DEV_PORT":3000 "$TAG"
+    docker run --rm -it -v "$PWD":/was-ui --name "$NAME" -p "$NEXT_DEV_PORT":3000 "$TAG"
 ;;
 
 *)
     echo "Passing unknown argument directly to container"
-    docker run --rm -it -v "$PWD":/was-ui -p "$NEXT_DEV_PORT":3000 "$TAG" "$@"
+    docker run --rm -it -v "$PWD":/was-ui --name "$NAME" -p "$NEXT_DEV_PORT":3000 "$TAG" "$@"
 ;;
 
 esac
