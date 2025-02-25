309dee34ee3b925449d0f2d2415d8a1520b900ca
Stijn Tintel <stijn@linux-ipv6.be>
2025-02-26 00:23:11 +0200

    pages/about: remove Discord widget
    This seems to throw a ton of errors in the browser console.

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,17 +20,6 @@ const About: NextPage = () => {
             <b>WAS:</b> {versionData?.was.version}
           </CardContent>
         </InformationCard>
-        <InformationCard title="Community Discord">
-          <CardContent sx={{ textAlign: 'center', padding: 1 }}>
-            <iframe
-              src="https://discord.com/widget?id=1161666824178503720&theme=dark"
-              height={500}
-              width={isDesktopOrLaptop ? '775px' : '345px'}
-              allowTransparency={true}
-              style={{ border: 0 }}
-              sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-          </CardContent>
-        </InformationCard>
       </Box>
     </LeftMenu>
   );
